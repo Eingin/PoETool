32a486057e034ffe4092294080219c62d068b910
Antonio <39470600+Eingin@users.noreply.github.com>
2020-09-19 01:26:08 +1000

    fix: Clean tree

--- tool/parsers/ggpk.py
+++ tool/parsers/ggpk.py
@@ -19,7 +19,7 @@ class FileEntry:
             self.entry = entry
             self.nameLength, = struct.unpack("I", reader.read(4))
             self.signature = struct.unpack("32B", reader.read(32))
-            self.name = reader.read(self.nameLength * 2).decode('utf-16')
+            self.name = reader.read(self.nameLength * 2).decode('utf-16').rstrip('\x00')
             self.dataOffset = reader.tell()
 
 class ChildEntry:
@@ -31,7 +31,7 @@ class PDirEntry:
         def __init__(self, entry: Entry, reader):
             self.nameLength, self.childCount, = struct.unpack("II", reader.read(8))
             self.signature = struct.unpack("32B", reader.read(32))
-            self.name = reader.read(self.nameLength * 2).decode('utf-16')
+            self.name = reader.read(self.nameLength * 2).decode('utf-16').rstrip('\x00')
             self.children = []
             for _ in range(self.childCount):
                 self.children.append(ChildEntry(reader))
@@ -53,16 +53,17 @@ def parseEntry(entry: Entry, reader):
 class GGPK:
     def __init__(self, reader):
         self.root = GGPKEntry(Entry(reader), reader)
-        self.tree = Node("GGPK")
 
     def render(self):
-        print(RenderTree(self.tree, maxlevel=3))
+        print(RenderTree(self.tree, maxlevel=2))
         
     def index(self, reader):
         for rootOffset in self.root.offsets:
             reader.seek(rootOffset)
             topLevelEntry = parseEntry(Entry(reader), reader)
-            self.indexEntry(topLevelEntry, self.tree, reader)
+            if type(topLevelEntry) is PDirEntry and not topLevelEntry.name:
+                topLevelEntry.name = "GGPK"
+                self.tree = self.indexEntry(topLevelEntry, None, reader)
             
     def indexEntry(self, entry, parentNode: Node, reader):
         if type(entry) is PDirEntry:
@@ -71,7 +72,9 @@ class GGPK:
                 reader.seek(child.offset)
                 childEntry = parseEntry(Entry(reader), reader)
                 self.indexEntry(childEntry, node, reader)
+            return node
         elif type(entry) is FileEntry:
             node = Node(entry.name, parent=parentNode)
+            return node
 
 
